6c943dae4c9501b70595fa4acd676e18f45f7484
Rodrigo Notario <rodrinot@ucm.es>
2020-06-25 01:16:34 +0200

    Parse JSON response.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -34,13 +34,16 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("4.1", "San Francisco", "04/07/2020"));
-        earthquakes.add(new Earthquake("4.1", "San Francisco", "04/07/2020"));
-        earthquakes.add(new Earthquake("4.1", "San Francisco", "04/07/2020"));
-        earthquakes.add(new Earthquake("4.1", "San Francisco", "04/07/2020"));
-        earthquakes.add(new Earthquake("4.1", "San Francisco", "04/07/2020"));
+        // Create a fake list of earthquakes.
+        /*ArrayList<Earthquake> earthquakes = new ArrayList<>();
+        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2, 2016"));
+        earthquakes.add(new Earthquake("6.1", "London", "July 20, 2015"));
+        earthquakes.add(new Earthquake("3.9", "Tokyo", "Nov 10, 2014"));
+        earthquakes.add(new Earthquake("5.4", "Mexico City", "May 3, 2014"));
+        earthquakes.add(new Earthquake("2.8", "Moscow", "Jan 31, 2013"));
+        earthquakes.add(new Earthquake("4.9", "Rio de Janeiro", "Aug 19, 2012"));
+        earthquakes.add(new Earthquake("1.6", "Paris", "Oct 30, 2011"));*/
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
